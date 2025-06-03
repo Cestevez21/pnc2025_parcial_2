0a556c85c0a7c684c18b3d12cd66510db3491516
cestevez21 <00120218@uca.edu.sv>
2025-06-02 18:25:00 -0600

    ADD: Baseline code

---
+++ pnc_parcial2/src/main/java/com/cestevez/pnc_parcial2/PncParcial2Application.java
@@ -0,0 +1,13 @@
+package com.cestevez.pnc_parcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncParcial2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PncParcial2Application.class, args);
+    }
+
+}
---
+++ pnc_parcial2/src/test/java/com/cestevez/pnc_parcial2/PncParcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cestevez.pnc_parcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncParcial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
